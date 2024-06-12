dca8272d095fcd759a19347b45ec8c30ffa70818
Cesar Tipan <cesar.tipanant@ug.edu.ec>
2024-06-12 00:56:33 -0500

    Agregue el form FrmPrincipal y solucione un error al iniciar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using POE_proyecto.Vista;
+
 namespace POE_proyecto
 {
     internal static class Program
@@ -10,8 +12,16 @@ namespace POE_proyecto
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            // TOCARA INVESTIGAR MEJOR ESTO, POR QUE NI IDEA POR QUE SE NECESITAN SETEAR ESTOS METODOS PRIMERO (Se saca y se rompe todo jaja)
+            // ESTOS COMENTARIOS HAY QUE BORRARLOS JAJA, SABES QUE ME DA COSA HACER TODO ESTO PARA QUE LUEGO VAYAMOS A HACER DIFERENTE, TIPO USAR ASP.NET AUNQUE NOSEEE
+            // LIT ES ALGO QUE RECOMENDARIA ANGELITO, PERO TOCARIA INVESTICAR BIEN QUE CHCH ES PRIMERO
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            // En .NETCore 8 se pueden inicializar objetos de esta manera
+            FrmPrincipal frmPrincipal = new();
+            Application.Run(frmPrincipal);
         }
     }
 }
