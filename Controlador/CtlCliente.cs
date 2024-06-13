7d2775aa993828fc1fdcc0548091f9a03a300a8e
Cesar Tipan <cesar.tipanant@ug.edu.ec>
2024-06-13 00:09:07 -0500

    CREATE: Controlador Principal y Controlador Cliente

---
+++ Controlador/CtlCliente.cs
@@ -0,0 +1,45 @@
+﻿using POE_proyecto.Datos;
+using POE_proyecto.Modelo;
+using POE_proyecto.Utilidades;
+
+namespace POE_proyecto.Controlador
+{
+    public class CtlCliente
+    {
+        #region fields
+        public CtlCliente() { }
+        #endregion
+
+        #region Methods
+        public IReadOnlyDictionary<int, Cliente> ObtenerClientes()
+        {
+            return AlmacenDeDatos.Clientes;
+        }
+
+        public bool AgregarCliente(int codigoCliente, string nombres, string apellidos, string cedula, string direccion, string numeroTelefono)
+        {
+            if (Validador.ValidarCedula(cedula) && Validador.ValidarTelefono(numeroTelefono))
+            {
+                Cliente nuevoCliente = new Cliente
+                {
+                    CodigoCliente = codigoCliente,
+                    Nombres = nombres,
+                    Apellidos = apellidos,
+                    Cedula = cedula,
+                    Direccion = direccion,
+                    NumeroTelefono = numeroTelefono
+                };
+
+                // Agregar cliente al almacen de datos
+                AlmacenDeDatos.AgregarCliente(codigoCliente, nuevoCliente);
+                return true;
+            }
+            else
+            {
+                // Manejar el caso de datos inválidos
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Controlador/CtlPrincipal.cs
@@ -0,0 +1,24 @@
+﻿using POE_proyecto.Datos;
+using POE_proyecto.Modelo;
+
+namespace POE_proyecto.Controlador
+{
+    public class CtlPrincipal
+    {
+        #region fields
+        private readonly CtlCliente ctlCliente;
+        #endregion
+
+        #region constructors
+        public CtlPrincipal()
+        {
+            ctlCliente = new CtlCliente();
+        }
+        #endregion
+
+        #region Properties
+
+        public CtlCliente CtlCliente { get { return ctlCliente; } }
+        #endregion
+    }
+}
