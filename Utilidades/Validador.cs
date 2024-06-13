6d2dfc901c668ba4c569e0db672d41e2e43d7c25
Cesar Tipan <cesar.tipanant@ug.edu.ec>
2024-06-13 00:10:13 -0500

    CREATE: Clase Validador

---
+++ Utilidades/Validador.cs
@@ -0,0 +1,17 @@
+﻿namespace POE_proyecto.Utilidades
+{
+    public static class Validador
+    {
+        public static bool ValidarCedula(string cedula)
+        {
+            // logica del validador
+            return true;
+        }
+
+        public static bool ValidarTelefono(string telefono)
+        {
+            // logica del validador
+            return true;
+        }
+    }
+}
