2a92178a8e2d9ae1ca301b0f48ff73e825fec700
Cesar Tipan <cesar.tipanant@ug.edu.ec>
2024-06-16 16:52:42 -0500

    ADD: Todos los controladores al controlador principal

--- Controlador/CtlPrincipal.cs
+++ Controlador/CtlPrincipal.cs
@@ -7,18 +7,26 @@ namespace POE_proyecto.Controlador
     {
         #region fields
         private readonly CtlCliente ctlCliente;
+        private readonly CtlMecanico ctlMecanico;
+        private readonly CtlMantenimiento ctlMantenimiento;
+        private readonly CtlVehiculo ctlVehiculo;
         #endregion
 
         #region constructors
         public CtlPrincipal()
         {
             ctlCliente = new();
+            ctlMecanico = new();
+            ctlMantenimiento = new();
+            ctlVehiculo = new();
         }
         #endregion
 
         #region Properties
-
         public CtlCliente CtlCliente { get { return ctlCliente; } }
+        public CtlMecanico CtlMecanico { get { return ctlMecanico; } }
+        public CtlMantenimiento CtlMantenimiento { get { return ctlMantenimiento; } }
+        public CtlVehiculo CtlVehiculo { get { return ctlVehiculo; } }
         #endregion
     }
 }
