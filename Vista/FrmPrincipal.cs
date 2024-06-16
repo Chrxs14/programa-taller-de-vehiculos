998554f12715f4881087994c04f91cc698f2f9e4
chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-06-16 14:47:21 -0500

    Created: Se cambiaron las vistas y se agregó Fontawesome para iconos en las vistas

--- Vista/FrmPrincipal.cs
+++ Vista/FrmPrincipal.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using FontAwesome.Sharp;
+using Color = System.Drawing.Color;
 
 namespace POE_proyecto.Vista
 {
     public partial class FrmPrincipal : Form
     {
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
+
+
         public FrmPrincipal()
         {
             InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(10, 60);
+
+            panelMenu.Controls.Add(leftBorderBtn);
+        }
+
+        //Structs
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(237, 239, 242);
+        }
+
+
+        //Methods
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if (senderBtn != null)
+            {
+                DisableButton();
+
+                //Button
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37, 36, 81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor = color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign = ContentAlignment.MiddleRight;
+
+                //left
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0, currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(36, 44, 69);
+                currentBtn.ForeColor = Color.White;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.White;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            }
         }
 
         private void FrmPrincipal_Load(object sender, EventArgs e)
@@ -25,6 +81,51 @@ namespace POE_proyecto.Vista
         private void nuevoClienteToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void iconButton1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton1_Click_2(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void panel2_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void vehicleManageBtn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void maintenanceManageBtn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void reportBtn_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
         }
     }
 }
